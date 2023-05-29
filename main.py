78f37a1a578553ba03db65cc192c6b4b5e9c12ad
Owen Shi <2008owenshi@gmail.com>
2023-05-29 15:50:09 -0400

    basic animations

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from sys import exit
 pygame.init()
 pygame.display.set_caption("Stickman")
 
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((800, 400), pygame.RESIZABLE)
 screen.fill((255, 255, 255))
 clock = pygame.time.Clock()
 font = pygame.font.Font('font/LDFComicSans.ttf', 50)
 
 player = pygame.sprite.GroupSingle(Player())
-exit = font.render('exit', None, 'Black')
-exit_rect = exit.get_rect(topleft=(500, 500))
 
 while 1:
     for event in pygame.event.get():
@@ -24,12 +22,6 @@ while 1:
 
     player.draw(screen)
     player.update()
-
-    screen.blit(exit, exit_rect)
-    mouse_pos = pygame.mouse.get_pos()
-    if exit_rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed() == (True, False, False):
-        pygame.quit()
-        exit()
 
     pygame.display.update()
     clock.tick(60)
--- sprites/player.py
+++ sprites/player.py
@@ -6,12 +6,14 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.run_animation = [pygame.transform.rotozoom(pygame.image.load("graphics/player/run/run_1.png").convert_alpha(), 0, 0.25), pygame.transform.rotozoom(pygame.image.load("graphics/player/run/run_2.png").convert_alpha(), 0, 0.25), pygame.transform.rotozoom(pygame.image.load(
             "graphics/player/run/run_3.png").convert_alpha(), 0, 0.25), pygame.transform.rotozoom(pygame.image.load("graphics/player/run/run_4.png").convert_alpha(), 0, 0.25), pygame.transform.rotozoom(pygame.image.load("graphics/player/run/run_5.png").convert_alpha(), 0, 0.25)]
-        self.jump_animation = [pygame.transform.rotozoom(pygame.image.load("graphics/player/jump/jump_1.png").convert_alpha(
-        ), 0, 0.25), pygame.transform.rotozoom(pygame.image.load("graphics/player/jump/jump_2.png").convert_alpha(), 0, 0.25),]
+        self.jump_animation = pygame.transform.rotozoom(pygame.image.load("graphics/player/jump/jump_1.png").convert_alpha(
+        ), 0, 0.25)
+        self.idle_animation = [pygame.transform.rotozoom(pygame.image.load("graphics/player/idle/idle_1.png").convert_alpha(), 0, 0.25), pygame.transform.rotozoom(
+            pygame.image.load("graphics/player/idle/idle_2.png").convert_alpha(), 0, 0.25)]
         self.image = self.run_animation[0]
         self.rect = self.image.get_rect()
         self.run_frame = 0
-        self.jump_frame = 0
+        self.idle_frame = 0
         self.gravity = 0
 
     def movement(self):
@@ -25,24 +27,37 @@ class Player(pygame.sprite.Sprite):
                 self.image = pygame.transform.flip(
                     self.run_animation[int(self.run_frame)], True, False)
 
-        def animate_jump():
-            self.jump_frame += 0.05
-            if self.jump_frame >= len(self.jump_animation):
-                self.jump_frame = 0
-            self.image = self.jump_animation[int(self.jump_frame)]
+        def animate_jump(direction):
+            if direction == "RIGHT":
+                self.image = self.jump_animation
+            elif direction == "LEFT":
+                self.image = pygame.transform.flip(
+                    self.jump_animation, True, False)
+
+        def animate_idle():
+            self.idle_frame += 0.04
+            if self.idle_frame >= len(self.idle_animation):
+                self.idle_frame = 0
+            self.image = self.idle_animation[int(self.idle_frame)]
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             animate_run("LEFT")
             self.rect.x -= 5
-        if keys[pygame.K_d]:
+        elif keys[pygame.K_d]:
             animate_run("RIGHT")
             self.rect.x += 5
+
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -15
 
         if self.rect.bottom < 300:
-            animate_jump()
+            if keys[pygame.K_a]:
+                animate_jump("LEFT")
+            else:
+                animate_jump("RIGHT")
+        elif not keys[pygame.K_a] and not keys[pygame.K_d]:
+            animate_idle()
 
     def apply_gravity(self):
         self.gravity += 1
